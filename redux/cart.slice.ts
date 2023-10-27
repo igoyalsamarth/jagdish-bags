f6bb845d32da5a54975a9ef4b77bbe4aa8dbb742
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-10-27 23:08:30 +0530

    redux-persist-setup

--- redux/cart.slice.ts
+++ redux/cart.slice.ts
@@ -1,27 +1,17 @@
 import { LocalCart } from "@/components/nonWovenTile";
 import { PayloadAction, createSlice, current } from "@reduxjs/toolkit";
 
-const initialState: Array<LocalCart> = [
-  {
-    "borderColor":null,
-    "borderGSM":0,
-    "handleColor":null,
-    "handleType":null
-  }
-]
-console.log(typeof initialState)
+interface ICartState{
+  cart:Array<LocalCart>;
+}
+const initialState: ICartState = {cart:[]}
+
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     addToCar: (state, action: PayloadAction<LocalCart>) => {
-      console.log(current(state));
-      console.log(action);
-      console.log(initialState);
-      console.log(typeof initialState)
-      state.push(action.payload)
-      //return [...state,{"borderColor":action.payload.borderColor,"borderGSM":action.payload.borderGSM,"handleColor":action.payload.handleColor,"handleType":action.payload.handleType}]
-      console.log(current(state));
+      state.cart.push(action.payload)
     }
   }
 })
