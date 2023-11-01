227ec463245f3789c5982b025b29f8a68417b8e5
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-11-01 20:15:15 +0530

    logo change

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export default async function Header() {
         <header className="flex w-full bg-[#fefffe] py-6 border-b-2 border-gray-200 justify-center items-center h-[74px]">
             <div className="flex justify-between 2xl:w-[1170px] xl:w-[962px] lg:w-[738px] md:w-[451px] sm:w-[95%] items-center px-4">
                 <div className="flex gap-14">
-                    <Image src='/next.svg' alt="" height={40} width={40} />
+                    <Image src='/jagdishLogo.jpg' alt="" height={40} width={100} />
                     <div className="flex gap-4">
                         <Link href='' className="font-semibold text-gray-500 hover:text-black">Home</Link>
                         <Link href='/shop' className="font-semibold text-gray-500 hover:text-black">Shop</Link>
