e389a9729b5d6eb340b52ddd8629ab75134eead4
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-11-03 23:54:27 +0530

    env changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,8 +21,8 @@ export default async function Page() {
                                 <path d="M21 8L17.4392 9.97822C15.454 11.0811 14.4614 11.6326 13.4102 11.8488C12.4798 12.0401 11.5202 12.0401 10.5898 11.8488C9.53864 11.6326 8.54603 11.0811 6.5608 9.97822L3 8M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                             <div className="flex flex-col text-center text-sm text-gray-500 font-semibold">
-                            <p>{process.env.NEXT_PUBLIC_EMAIL_ONE}</p>
-                            <p>{process.env.NEXT_PUBLIC_EMAIL_TWO}</p>
+                            <p>{process.env.EMAIL_ONE}</p>
+                            <p>{process.env.EMAIL_TWO}</p>
                             </div>
                         </div>
                         <div className="flex flex-col col-span-4 sm:col-span-6 md:col-span-6 items-center gap-6 border border-gray-200 p-4 rounded-lg justify-around">
@@ -32,8 +32,8 @@ export default async function Page() {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M13.4857 5.3293C13.5995 4.93102 14.0146 4.7004 14.4129 4.81419L14.2069 5.53534C14.4129 4.81419 14.4129 4.81419 14.4129 4.81419L14.4144 4.81461L14.4159 4.81505L14.4192 4.81602L14.427 4.81834L14.4468 4.8245C14.4618 4.82932 14.4807 4.8356 14.5031 4.84357C14.548 4.85951 14.6074 4.88217 14.6802 4.91337C14.8259 4.97581 15.0249 5.07223 15.2695 5.21694C15.7589 5.50662 16.4271 5.9878 17.2121 6.77277C17.9971 7.55775 18.4782 8.22593 18.7679 8.7154C18.9126 8.95991 19.009 9.15897 19.0715 9.30466C19.1027 9.37746 19.1254 9.43682 19.1413 9.48173C19.1493 9.50418 19.1555 9.52301 19.1604 9.53809L19.1665 9.55788L19.1688 9.56563L19.1698 9.56896L19.1702 9.5705C19.1702 9.5705 19.1707 9.57194 18.4495 9.77798L19.1707 9.57194C19.2845 9.97021 19.0538 10.3853 18.6556 10.4991C18.2607 10.6119 17.8492 10.3862 17.7313 9.99413L17.7276 9.98335C17.7223 9.96832 17.7113 9.93874 17.6928 9.89554C17.6558 9.8092 17.5887 9.66797 17.4771 9.47938C17.2541 9.10264 16.8514 8.53339 16.1514 7.83343C15.4515 7.13348 14.8822 6.73078 14.5055 6.50781C14.3169 6.39619 14.1757 6.32909 14.0893 6.29209C14.0461 6.27358 14.0165 6.26254 14.0015 6.25721L13.9907 6.25352C13.5987 6.13564 13.3729 5.72419 13.4857 5.3293Z" fill="#000000" />
                             </svg>
                             <div className="flex flex-col text-center text-sm text-gray-500 font-semibold">
-                            <p><span className="text-gray-400">Varun Goyal:{' '}</span><span className=" whitespace-nowrap"><a href={`tel:${process.env.NEXT_PUBLIC_PHONE_ONE}`}>{process.env.NEXT_PUBLIC_PHONE_ONE}</a></span></p>
-                            <p><span className="text-gray-400">Manish Goyal:{' '}</span><span className=" whitespace-nowrap"><a href={`tel:${process.env.NEXT_PUBLIC_PHONE_TWO}`}>{process.env.NEXT_PUBLIC_PHONE_ONE}</a></span></p>
+                            <p><span className="text-gray-400">Varun Goyal:{' '}</span><span className=" whitespace-nowrap"><a href={`tel:${process.env.PHONE_ONE}`}>{process.env.PHONE_ONE}</a></span></p>
+                            <p><span className="text-gray-400">Manish Goyal:{' '}</span><span className=" whitespace-nowrap"><a href={`tel:${process.env.PHONE_TWO}`}>{process.env.PHONE_ONE}</a></span></p>
                             </div>
                         </div>
                     </div>
